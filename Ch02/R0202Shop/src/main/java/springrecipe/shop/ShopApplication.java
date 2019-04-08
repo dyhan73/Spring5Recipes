3ac826e342fe132f1ccf310ab3b054438e3a26f5
한대영 <dyhan@sk.com>
2019-04-08 19:37:34 +0900

    complete Recipe 2-2 but nothing new

---
+++ Ch02/R0202Shop/src/main/java/springrecipe/shop/ShopApplication.java
@@ -0,0 +1,23 @@
+package springrecipe.shop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class ShopApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(ShopApplication.class, args);
+        ApplicationContext context = new AnnotationConfigApplicationContext(ShopConfig.class);
+
+        Product aaa = context.getBean("aaa", Product.class);
+        Product cdrw = context.getBean("cdrw", Product.class);
+
+        System.out.println(aaa);
+        System.out.println(cdrw);
+    }
+
+}
