3b5d27b73cde22169f558a7bc3143b3407fcc19d
한대영 <dyhan@sk.com>
2019-04-08 16:17:26 +0900

    DI using ApplicationContext in main()

--- Ch02/R0201Sequence/src/main/java/springrecipes/sequence/SequenceApplication.java
+++ Ch02/R0201Sequence/src/main/java/springrecipes/sequence/SequenceApplication.java
@@ -2,12 +2,20 @@ package springrecipes.sequence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class SequenceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SequenceApplication.class, args);
-	}
+//		SpringApplication.run(SequenceApplication.class, args);
 
+		ApplicationContext context = new AnnotationConfigApplicationContext(SequenceGeneratorConfiguration.class);
+//		SequenceGenerator sequenceGenerator = (SequenceGenerator) context.getBean("sequenceGenerator");
+		SequenceGenerator sequenceGenerator = context.getBean("sequenceGenerator", SequenceGenerator.class);
+
+		System.out.println(sequenceGenerator.getSequence());
+		System.out.println(sequenceGenerator.getSequence());
+	}
 }
