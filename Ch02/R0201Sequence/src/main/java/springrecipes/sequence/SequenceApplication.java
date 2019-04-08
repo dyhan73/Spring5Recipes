574276f6908382b4d0febe9e3f27548df4a3692f
한대영 <dyhan@sk.com>
2019-04-08 19:07:28 +0900

    add SequenceDao and using POJO via IoC

--- Ch02/R0201Sequence/src/main/java/springrecipes/sequence/SequenceApplication.java
+++ Ch02/R0201Sequence/src/main/java/springrecipes/sequence/SequenceApplication.java
@@ -1,9 +1,12 @@
 package springrecipes.sequence;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
 public class SequenceApplication {
@@ -17,5 +20,11 @@ public class SequenceApplication {
 
 		System.out.println(sequenceGenerator.getSequence());
 		System.out.println(sequenceGenerator.getSequence());
+
+		ApplicationContext context2 = new AnnotationConfigApplicationContext("springrecipes.sequence");
+		SequenceDao sequenceDao = context2.getBean(SequenceDao.class);
+
+		System.out.println(sequenceDao.getNextValue("IT"));
+		System.out.println(sequenceDao.getNextValue("IT"));
 	}
 }
