51d5e859957688ca660c723ef888cb372ba3050c
한대영 <dyhan@sk.com>
2019-04-14 15:52:46 +0900

    [Recipe 2.8] Lazy initial using @Lazy

--- Ch02/R0208-BeanLifeCycle/src/main/java/springrecipes/r0208/ShoppingCart.java
+++ Ch02/R0208-BeanLifeCycle/src/main/java/springrecipes/r0208/ShoppingCart.java
@@ -1,5 +1,6 @@
 package springrecipes.r0208;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import springrecipes.r0208.domain.Product;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Component
 @Scope("prototype")
+@Lazy
 public class ShoppingCart {
 
     private List<Product> items = new ArrayList<>();
