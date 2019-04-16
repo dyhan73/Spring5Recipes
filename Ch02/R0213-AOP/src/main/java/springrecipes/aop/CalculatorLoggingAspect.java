2dd6fb7038f63151abe8f826c40d4e71c1e1b652
한대영 <dyhan@sk.com>
2019-04-16 20:28:55 +0900

    [Recipe 2.15] get JoinPoint info.

--- Ch02/R0213-AOP/src/main/java/springrecipes/aop/CalculatorLoggingAspect.java
+++ Ch02/R0213-AOP/src/main/java/springrecipes/aop/CalculatorLoggingAspect.java
@@ -3,8 +3,8 @@ package springrecipes.aop;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -15,24 +15,35 @@ public class CalculatorLoggingAspect {
 
     private Log log = LogFactory.getLog(this.getClass());
 
-    @Around("execution(* springrecipes.aop.*.*(..))")
-    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.info(String.format("The method %s() begins with %s"
-                , joinPoint.getSignature().getName()
-                , Arrays.toString(joinPoint.getArgs())));
-
-        try {
-            Object result = joinPoint.proceed();
-            log.info(String.format("The method %s() ends with %s"
-                    , joinPoint.getSignature().getName()
-                    , result));
-            return result;
-        } catch (IllegalArgumentException e) {
-            log.error(String.format("Illegal argument %s in %s()",
-                    Arrays.toString(joinPoint.getArgs()), joinPoint.getSignature().getName()));
-            throw e;
-        }
+    @Before("execution(* springrecipes.aop.*.*(..))")
+    public void logJoinPoint(JoinPoint joinPoint) {
+        log.info("Join point kind : " + joinPoint.getKind());
+        log.info("Signature declaring type : " + joinPoint.getSignature().getDeclaringTypeName());
+        log.info("Signature name : " + joinPoint.getSignature().getName());
+        log.info("Arguments : " + Arrays.toString(joinPoint.getArgs()));
+        log.info("Target class : " + joinPoint.getTarget().getClass().getName());
+        log.info("This class : " + joinPoint.getThis().getClass().getName());
     }
+
+
+//    @Around("execution(* springrecipes.aop.*.*(..))")
+//    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+//        log.info(String.format("The method %s() begins with %s"
+//                , joinPoint.getSignature().getName()
+//                , Arrays.toString(joinPoint.getArgs())));
+//
+//        try {
+//            Object result = joinPoint.proceed();
+//            log.info(String.format("The method %s() ends with %s"
+//                    , joinPoint.getSignature().getName()
+//                    , result));
+//            return result;
+//        } catch (IllegalArgumentException e) {
+//            log.error(String.format("Illegal argument %s in %s()",
+//                    Arrays.toString(joinPoint.getArgs()), joinPoint.getSignature().getName()));
+//            throw e;
+//        }
+//    }
 
 //    @Before("execution(* springrecipes.aop.*.*(..))")
 //    public void logBefore(JoinPoint joinPoint) {
