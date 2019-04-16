1aef6eb273d8c0668ea8afeafda1725c202c3925
한대영 <dyhan@sk.com>
2019-04-16 16:53:15 +0900

    [Recipe 2.13] improve @Before advice with JoinPoint param

--- Ch02/R0213-AOP/src/main/java/springrecipes/aop/CalculatorLoggingAspect.java
+++ Ch02/R0213-AOP/src/main/java/springrecipes/aop/CalculatorLoggingAspect.java
@@ -2,9 +2,12 @@ package springrecipes.aop;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -12,8 +15,9 @@ public class CalculatorLoggingAspect {
 
     private Log log = LogFactory.getLog(this.getClass());
 
-    @Before("execution(* ArithmeticCalculator.add(..))")
-    public void logBefore() {
-        log.info("The method Add() begins");
+    @Before("execution(* springrecipes.aop.*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        log.info("The method " + joinPoint.getSignature().getName() +
+                "() begins with " + Arrays.toString(joinPoint.getArgs()));
     }
 }
