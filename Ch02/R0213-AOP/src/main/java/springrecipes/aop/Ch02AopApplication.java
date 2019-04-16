d25729f40f9abb105e7cd88b565e711d78a488a7
한대영 <dyhan@sk.com>
2019-04-16 13:53:32 +0900

    [Recipe 2.13] init new module for testing Spring AOP

---
+++ Ch02/R0213-AOP/src/main/java/springrecipes/aop/Ch02AopApplication.java
@@ -0,0 +1,13 @@
+package springrecipes.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch02AopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch02AopApplication.class, args);
+    }
+
+}
