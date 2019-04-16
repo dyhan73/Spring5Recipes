7eb46df7f91a8cf8c5f711e1778f41afadfbdb5b
한대영 <dyhan@sk.com>
2019-04-16 18:14:45 +0900

    [Recipe 2.13] replace advices to @Around advice which is powerful but danger

--- Ch02/R0213-AOP/src/main/java/springrecipes/aop/CalculatorLoggingAspect.java
+++ Ch02/R0213-AOP/src/main/java/springrecipes/aop/CalculatorLoggingAspect.java
@@ -3,6 +3,7 @@ package springrecipes.aop;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
@@ -14,28 +15,47 @@ public class CalculatorLoggingAspect {
 
     private Log log = LogFactory.getLog(this.getClass());
 
-    @Before("execution(* springrecipes.aop.*.*(..))")
-    public void logBefore(JoinPoint joinPoint) {
-        log.info("The method " + joinPoint.getSignature().getName() +
-                "() begins with " + Arrays.toString(joinPoint.getArgs()));
+    @Around("execution(* springrecipes.aop.*.*(..))")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info(String.format("The method %s() begins with %s"
+                , joinPoint.getSignature().getName()
+                , Arrays.toString(joinPoint.getArgs())));
+
+        try {
+            Object result = joinPoint.proceed();
+            log.info(String.format("The method %s() ends with %s"
+                    , joinPoint.getSignature().getName()
+                    , result));
+            return result;
+        } catch (IllegalArgumentException e) {
+            log.error(String.format("Illegal argument %s in %s()",
+                    Arrays.toString(joinPoint.getArgs()), joinPoint.getSignature().getName()));
+            throw e;
+        }
     }
 
-    @After("execution(* springrecipes.aop.*.*(..))")
-    public void logAfter(JoinPoint joinPoint) {
-        log.info("The method " + joinPoint.getSignature().getName() + "() ends");
-    }
-
-    @AfterReturning(
-            pointcut = "execution(* springrecipes.aop.*.*(..))",
-            returning = "result")
-    public void logAfterReturning(JoinPoint joinPoint, Object result) {
-        log.info("The method " + joinPoint.getSignature().getName() + "() ends with " + result);
-    }
-
-    @AfterThrowing(
-            pointcut = "execution(* springrecipes.aop.*.*(..))",
-            throwing = "e")
-    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
-        log.error("An exception " + e + " has been thrown in " + joinPoint.getSignature().getName() + "()");
-    }
+//    @Before("execution(* springrecipes.aop.*.*(..))")
+//    public void logBefore(JoinPoint joinPoint) {
+//        log.info("The method " + joinPoint.getSignature().getName() +
+//                "() begins with " + Arrays.toString(joinPoint.getArgs()));
+//    }
+//
+//    @After("execution(* springrecipes.aop.*.*(..))")
+//    public void logAfter(JoinPoint joinPoint) {
+//        log.info("The method " + joinPoint.getSignature().getName() + "() ends");
+//    }
+//
+//    @AfterReturning(
+//            pointcut = "execution(* springrecipes.aop.*.*(..))",
+//            returning = "result")
+//    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+//        log.info("The method " + joinPoint.getSignature().getName() + "() ends with " + result);
+//    }
+//
+//    @AfterThrowing(
+//            pointcut = "execution(* springrecipes.aop.*.*(..))",
+//            throwing = "e")
+//    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+//        log.error("An exception " + e + " has been thrown in " + joinPoint.getSignature().getName() + "()");
+//    }
 }
